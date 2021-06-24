6fdb13a16955781d1795789b3cb023d34613bceb
伊藤 翔 <sito@dai-ichi-lab.com>
2021-06-24 21:46:31 +0900

    機能追加（フィルター選択まで）

--- src/views/Master.vue
+++ src/views/Master.vue
@@ -1,12 +1,27 @@
 <template>
-  <h1>親ページ</h1>
+ <video ref="theirRef" id="video" autoplay muted playsinline></video>
+ <div class="peerhead">
   <section>
-    <h2>相手のvideo</h2>
-    <p>Peer ID: {{ currentPeer.id }}</p>
-    <video ref="theirRef" width="400" autoplay muted playsinline></video>
+    <p>Peer ID: {{ currentPeer.id }}</p>    
   </section>
-  <button @click="initSkyway">カメラの初期化</button>
-  <router-link to="/puppet">子ページ</router-link>
+  <button @click="initSkyway" class="btn-init-cam">initialize</button>
+  <router-link to="/puppet">move puppet</router-link>
+ </div>
+      <div class="container">
+        <img src="@/assets/cockpit8.png"  class="overlay" v-if="prefilter1"/>
+        <img src="@/assets/cockpit7.png"  class="overlay" v-if="prefilter2"/>
+      </div>
+            <div class="ui">
+            <a href="#" v-on:click="changefilter1" class="btn-circle-3d-dent">ChangeFilter-1</a>
+            <a href="#" v-on:click="changefilter2" class="btn-circle-3d-dent">ChangeFilter-2</a>
+            <a href="#" v-on:click="changemodel" class="btn-circle-3d-dent">3D-Cockpit</a>
+            <!-- <a href="#" v-on:click="uploadfilter" class="btn-circle-3d-dent">UploadFilter</a> -->
+            <label class="btn-circle-3d-dent">
+            <input ref="file" class="file-button" type="file" @change="uploadfilter" />
+            UploadFilter
+            </label>
+            <a href="#" v-on:click="changefullscreen" class="btn-circle-3d-dent">ChangeFullScreen</a>
+      </div>
 </template>
 
 <script>
@@ -20,7 +35,7 @@ export default {
     const localStream = ref(null);
     const currentPeer = ref({})
     const theirId = ref('')
-
+    
     const initSkyway = () => {
       const peer = new Peer({key: process.env.VUE_APP_API_KEY, debug: 3});
 
@@ -46,8 +61,169 @@ export default {
       currentPeer,
       theirRef,
       theirId,
-      initSkyway,
+      initSkyway      
+    }
+  },
+  data(){
+    return{
+      prefilter1: false,
+      prefilter2: false
+    }
+  },
+  methods: {
+    changefilter1(){
+      this.prefilter1 = true;
+      this.prefilter2 = false;
+    },
+    changefilter2(){
+      this.prefilter1 = false;
+      this.prefilter2 = true;      
     }
   }
 }
 </script>
+
+<style>
+  #canvas {
+    display: none;
+  }
+
+  #video {
+  position: fixed; right: 0; bottom: 0;
+  min-width: 100%; min-height: 100%;
+  width: auto; height: auto; z-index: -100;
+  background-size: cover;
+  }
+
+  #app{
+    max-width: 100vw; max-height: 100vh;
+  }
+
+  .container{
+    max-width: 100%; max-height: 100%;
+    width: 100vw; height: 100vh;
+  }
+
+  .overlay {
+  position: absolute; top: 50%; left:50%;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  min-width: 100%; min-height: 100%;
+  width: 100%; height: 100%; 
+  object-fit: cover;
+  z-index: -50;
+  }
+
+  .capture {
+    /* display: inline; */
+    padding: 5px;
+  }
+
+  .peerhead {
+    position: absolute;
+    z-index: 100;
+    top: 5%;
+    left: 0;
+    width: 100%;
+    height: auto;
+    margin: 0;
+    padding: 10px 15px 30px;
+    text-align: left;
+    vertical-align: center;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Orbitron', sans-serif;
+    text-decoration: none;
+    background: #4ec99000;
+    color: rgb(100, 160, 77);
+    text-shadow: 1px 1px 1px rgb(0, 27, 6);
+    }
+
+    .btn-init-cam{
+    font-family: 'Orbitron', sans-serif;
+    text-decoration: none;
+    background: #4ec99000;
+    color: rgb(100, 160, 77);
+    width: 120;
+    font-size: 16px;
+    height: auto;
+    line-height: 50px;
+    text-align: left;
+    overflow:auto;
+    font-weight: bold;
+    text-shadow: 2px 2px 2px rgb(0, 27, 6);
+    transition: .4s;
+    border: none;
+    }
+
+   .ui {
+    position: absolute;
+    z-index: 100;
+    bottom: 5%;
+    left: 0;
+    width: 100%;
+    height: auto;
+    margin: 0;
+    padding: 10px 15px 30px;
+    text-align: left;
+    vertical-align: center;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    }
+
+    .btn-circle-3d-dent {
+    font-family: 'Orbitron', sans-serif;
+    text-decoration: none;
+    background: #4ec99000;
+    color: rgb(100, 160, 77);
+    width: 100;
+    font-size: 16px;
+    height: 50px;
+    line-height: 50px;
+    text-align: left;
+    overflow:auto;
+    font-weight: bold;
+    text-shadow: 2px 2px 2px rgb(0, 27, 6);
+    transition: .4s;
+  }
+
+    .ui2 {
+    position: absolute;
+    z-index: 100;
+    top: 5%;
+    right: 0%;
+    width: 100%;
+    height: auto;
+    margin: 0;
+    padding: 10px 15px 30px;
+    text-align: right;
+    vertical-align: center;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    }
+
+    .btn-circle-3d-dent2 {
+    font-family: 'Orbitron', sans-serif;
+    text-decoration: none;
+    background: #4ec99000;
+    color: rgb(100, 160, 77);
+    width: 100;
+    font-size: 16px;
+    height: 50px;
+    line-height: 50px;
+    text-align: right;
+    overflow:auto;
+    font-weight: bold;
+    text-shadow: 2px 2px 2px rgb(0, 27, 6);
+    transition: .4s;
+  }
+
+  .file-button {
+    display: none;
+  }
+
+</style>
